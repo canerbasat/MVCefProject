2ea931b936cb1579b17d46cdf3b96e963f695872
canerbasat <canerbasat96@gmail.com>
2019-07-16 13:51:31 +0300

    Project pushed

---
+++ NetCampExample/Catalog/Catalog.DataAccess/EFCoreContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catalog.DataAccess
+{
+    public class EFCoreContext:DbContext
+    {
+        public EFCoreContext(DbContextOptions<EFCoreContext> options):base(options)
+        {
+
+        }
+
+        public EFCoreContext()
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseMySQL(@"Server=localhost;Database=CampTest;User=root;Password=47854785zZ.;");
+            base.OnConfiguring(optionsBuilder);
+
+        }
+    }
+}
